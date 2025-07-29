c45cb9196a07e1ba2619eb3c93d6b66b0b84270a
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2025-07-29 11:26:17 -0500

    Continue refactoring
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

--- collection-scripts/utilities/utilities.sh
+++ collection-scripts/utilities/utilities.sh
@@ -26,28 +26,28 @@ echoInfo() {
 
 # Same as echoInfo but only does anything if the log level is greater than or equal to 2
 echoVerbose() {
-  if [ ${OPTIONS[log]} -ge 2 ]; then
+  if [[ ${OPTIONS[log]} -ge 2 ]]; then
     echoInfo "${@}"
   fi
 }
 
 # Same as echoInfo but only does anything if the log level is greater than or equal to 3
 echoVerbose3() {
-  if [ ${OPTIONS[log]} -ge 3 ]; then
+  if [[ ${OPTIONS[log]} -ge 3 ]]; then
     echoInfo "${@}"
   fi
 }
 
 # Same as echoInfo but only does anything if the log level is greater than or equal to 3
 echoVerbose4() {
-  if [ ${OPTIONS[log]} -ge 4 ]; then
+  if [[ ${OPTIONS[log]} -ge 4 ]]; then
     echoInfo "${@}"
   fi
 }
 
 # Same as echoInfo but only does anything if the log level is greater than or equal to 3
 echoVerbose5() {
-  if [ ${OPTIONS[log]} -ge 5 ]; then
+  if [[ ${OPTIONS[log]} -ge 5 ]]; then
     echoInfo "${@}"
   fi
 }
@@ -60,7 +60,7 @@ echoLarge() {
 
 # Same as echoLarge but only does anything if the log level is greater than 1
 echoLargeVerbose() {
-  if [ ${OPTIONS[log]} -gt 1 ]; then
+  if [[ ${OPTIONS[log]} -gt 1 ]]; then
     echoLarge "${@}"
   fi
 }
@@ -77,7 +77,7 @@ endRun() {
 
   # Check if PID array has any values, if so, wait for them to finish
   echoInfo "Waiting on BACKGROUND_PIDS array: [${BACKGROUND_PIDS[@]}]"
-  if [ ${#BACKGROUND_PIDS[@]} -ne 0 ]; then
+  if [[ ${#BACKGROUND_PIDS[@]} -ne 0 ]]; then
     wait "${BACKGROUND_PIDS[@]}"
   fi
 
@@ -90,8 +90,12 @@ endRun() {
 }
 
 ensureAllPodsJSON() {
-  if [ "${ALL_PODS_JSON}" = "" ]; then
+  if [[ "${ALL_PODS_JSON}" = "" ]]; then
+    echoInfo "Gathering all pods JSON"
+
     ALL_PODS_JSON="$(oc get pods -A -o json)"
+
+    echoInfo "Finsished gathering all pods JSON. Lines: $(echo "${ALL_PODS_JSON}" | wc -l)"
 
     # This is too heavy and causes issues with oc
     echoLargeVerbose "ALL_PODS_JSON = ${ALL_PODS_JSON}"
@@ -150,7 +154,7 @@ getPodLogsByAppName() {
   ensureAllPodsJSON
   INPUT="$(echo "${ALL_PODS_JSON}" | jq -r ".items[] | select(.metadata.labels.app == \"${SEARCH}\") | [.metadata.namespace, .metadata.name] | @tsv")"
   echoVerbose "Pods = ${INPUT}"
-  if [ "${INPUT}" != "" ]; then
+  if [[ "${INPUT}" != "" ]]; then
     while IFS=$'\t' read -r ns pod; do
       getPodLogs "${ns}" "${pod}"
     done <<< "${INPUT}" # Piping into the while loop would be a subshell and would lose any variable updates (e.g. BACKGROUND_PIDS)
@@ -159,8 +163,109 @@ getPodLogsByAppName() {
 }
 
 defaultProcessing() {
-  # resource groups list
-  group_resources=(nodes pods events securitycontextconstraints)
+
+  echoInfo "Started defaultProcessing"
+
+  if [[ ${OPTIONS[all-pods-json]} = true ]]; then
+    ensureAllPodsJSON
+  fi
+
+  # Named resource list, eg. ns/openshift-config
+  named_resources=()
+
+  # Resource groups list, eg. pods
+  group_resources=()
+
+  # Resources to gather with `--all-namespaces` option
+  all_ns_resources=()
+  
+  if [[ ${OPTIONS[resources-nodes-allnamespaces]} = true ]]; then
+    all_ns_resources+=(nodes)
+  fi
+  if [[ ${OPTIONS[resources-pods-allnamespaces]} = true ]]; then
+    all_ns_resources+=(pods)
+  fi
+  if [[ ${OPTIONS[resources-events-allnamespaces]} = true ]]; then
+    all_ns_resources+=(events)
+  fi
+  if [[ ${OPTIONS[resources-securitycontextconstraints-allnamespaces]} = true ]]; then
+    all_ns_resources+=(securitycontextconstraints)
+  fi
+
+  # Cluster Version Information
+  if [[ ${OPTIONS[cluster-version-resources]} = true ]]; then
+    named_resources+=(ns/openshift-cluster-version)
+    group_resources+=(clusterversion)
+  fi
+
+  # Operator and APIService Resources
+  if [[ ${OPTIONS[operator-apiservice-resources]} = true ]]; then
+    group_resources+=(clusteroperators apiservices)
+  fi
+
+  # Certificate Resources
+  if [[ ${OPTIONS[certificate-resources]} = true ]]; then
+    group_resources+=(certificatesigningrequests)
+  fi
+
+  # Machine/Node Resources
+  if [[ ${OPTIONS[node-resources]} = true ]]; then
+    group_resources+=(nodes)
+  fi
+
+  # Namespaces/Project Resources
+  if [[ ${OPTIONS[namespace-resources]} = true ]]; then
+    named_resources+=(ns/default ns/openshift ns/kube-system ns/openshift-etcd)
+  fi
+
+  # Storage Resources
+  if [[ ${OPTIONS[storage-resources]} = true ]]; then
+    group_resources+=(storageclasses persistentvolumes volumeattachments csidrivers csinodes volumesnapshotclasses volumesnapshotcontents clustercsidrivers)
+    all_ns_resources+=(csistoragecapacities)
+  fi
+
+  # Image-source Resources
+  if [[ ${OPTIONS[image-source-resources]} = true ]]; then
+    group_resources+=(imagecontentsourcepolicies.operator.openshift.io)
+  fi
+
+  # Networking Resources
+  if [[ ${OPTIONS[networking-resources]} = true ]]; then
+    group_resources+=(networks.operator.openshift.io)
+  fi
+
+  # NodeNetworkState
+  if [[ ${OPTIONS[nodenetworkstates]} = true ]]; then
+    resources+=(nodenetworkstates nodenetworkconfigurationenactments nodenetworkconfigurationpolicies)
+  fi
+
+  # Assisted Installer
+  if [[ ${OPTIONS[assisted-installer]} = true ]]; then
+    named_resources+=(ns/assisted-installer)
+  fi
+
+  # Leases
+  if [[ ${OPTIONS[leases]} = true ]]; then
+    all_ns_resources+=(leases)
+  fi
+
+  # Flowcontrol - API Priority and Fairness (APF)
+  if [[ ${OPTIONS[flowcontrol]} = true ]]; then
+    group_resources+=(prioritylevelconfigurations.flowcontrol.apiserver.k8s.io flowschemas.flowcontrol.apiserver.k8s.io)
+  fi
+
+  # ClusterResourceQuota
+  if [[ ${OPTIONS[clusterresourcequotas]} = true ]]; then
+    group_resources+=(clusterresourcequotas.quota.openshift.io)
+  fi
+
+  # Run the Collection of Resources using inspect
+  # running across all-namespaces for the few "Autoscaler" resources.
+  if [[ ${#named_resources[@]} -ne 0 ]]; then
+    echoInfo "Requesting named resources ${named_resources[@]}"
+    oc adm inspect ${log_collection_args} --dest-dir "${MGOUTPUT}" --rotated-pod-logs "${named_resources[@]}" &
+    queuedBackground $!
+  fi
 
   filtered_group_resources=()
   for gr in "${group_resources[@]}"
@@ -170,25 +275,151 @@ defaultProcessing() {
       filtered_group_resources+=("$gr")
     fi
   done
-
   group_resources_text=$(IFS=, ; echo "${filtered_group_resources[*]}")
-
-  echoInfo "Calling main oc adm inspect with ${group_resources_text}"
-
-  oc adm inspect --dest-dir "${MGOUTPUT}" --rotated-pod-logs "${group_resources_text}" --all-namespaces &
-  queuedBackground $!
-
-  echoInfo "Sparked off main oc adm inspect; BACKGROUND_PIDS = ${BACKGROUND_PIDS[@]}"
-
-  if [ ${OPTIONS[all-pods-json]} = true ]; then
-    ensureAllPodsJSON
+  if [[ "${group_resources_text}" != "" ]]; then
+    echoInfo "Requesting group resources ${group_resources_text}"
+    oc adm inspect ${log_collection_args} --dest-dir "${MGOUTPUT}" --rotated-pod-logs "${group_resources_text}" &
+    queuedBackground $!
   fi
 
-  if [ ${OPTIONS[logs-crashloopbackoff]} = true ]; then
+  all_ns_resources_text=$(IFS=, ; echo "${all_ns_resources[*]}")
+  if [[ "${all_ns_resources_text}" != "" ]]; then
+    echoInfo "Requesting all namespace resources ${all_ns_resources_text}"
+    oc adm inspect ${log_collection_args} --dest-dir "${MGOUTPUT}" --rotated-pod-logs "${all_ns_resources_text}" --all-namespaces &
+    queuedBackground $!
+  fi
+
+  # Gather Insights Operator Archives
+  if [[ ${OPTIONS[insights-operator]} = true ]]; then
+    /usr/bin/gather_insights &
+    queuedBackground $!
+  fi
+
+  # Gather monitoring data from the cluster
+  if [[ ${OPTIONS[monitoring-data]} = true ]]; then
+    /usr/bin/gather_monitoring &
+    queuedBackground $!
+  fi
+
+  # Gather optional operator resources from all namespaces
+  if [[ ${OPTIONS[olm-resources]} = true ]]; then
+    /usr/bin/gather_olm &
+    queuedBackground $!
+  fi
+
+  # Gather API Priority and Fairness Endpoints
+  if [[ ${OPTIONS[api-priority-fairness]} = true ]]; then
+    /usr/bin/gather_priority_and_fairness &
+    queuedBackground $!
+  fi
+
+  # Gather etcd information
+  if [[ ${OPTIONS[etcd]} = true ]]; then
+    /usr/bin/gather_etcd &
+    queuedBackground $!
+  fi
+
+  # Gather Service Logs (using a supplemental Script); Scoped to Masters.
+  if [[ ${OPTIONS[service-logs]} = true ]]; then
+    /usr/bin/gather_service_logs master &
+    queuedBackground $!
+  fi
+
+  # Gather Windows Kubernetes component logs
+  if [[ ${OPTIONS[windows-node-logs]} = true ]]; then
+    /usr/bin/gather_windows_node_logs &
+    queuedBackground $!
+  fi
+
+  # Gather HAProxy config files
+  if [[ ${OPTIONS[haproxy-config]} = true ]]; then
+    /usr/bin/gather_haproxy_config &
+    queuedBackground $!
+  fi
+
+  # Gather kas startup and termination logs
+  if [[ ${OPTIONS[kas-startup-termination]} = true ]]; then
+    /usr/bin/gather_kas_startup_termination_logs &
+    queuedBackground $!
+  fi
+
+  # Gather network logs
+  if [[ ${OPTIONS[logs-network]} = true ]]; then
+    /usr/bin/gather_network_logs_basics &
+    queuedBackground $!
+  fi
+
+  # Gather metallb logs
+  if [[ ${OPTIONS[logs-metallb]} = true ]]; then
+    /usr/bin/gather_metallb &
+    queuedBackground $!
+  fi
+
+  # Gather frr-k8s logs
+  if [[ ${OPTIONS[logs-frr-k8s]} = true ]]; then
+    /usr/bin/gather_frrk8s &
+    queuedBackground $!
+  fi
+
+  # Gather NMState
+  if [[ ${OPTIONS[nmstate]} = true ]]; then
+    /usr/bin/gather_nmstate &
+    queuedBackground $!
+  fi
+
+  # Gather SR-IOV resources
+  if [[ ${OPTIONS[sriov]} = true ]]; then
+    /usr/bin/gather_sriov &
+    queuedBackground $!
+  fi
+
+  # Gather PodNetworkConnectivityCheck
+  if [[ ${OPTIONS[podnetworkconnectivitycheck]} = true ]]; then
+    /usr/bin/gather_podnetworkconnectivitycheck &
+    queuedBackground $!
+  fi
+
+  # Gather On-Disk MachineConfig files
+  if [[ ${OPTIONS[machineconfig-ondisk]} = true ]]; then
+    /usr/bin/gather_machineconfig_ondisk &
+    queuedBackground $!
+  fi
+
+  # Gather On-Disk MachineConfigDaemon logs
+  if [[ ${OPTIONS[logs-machineconfig-termination]} = true ]]; then
+    /usr/bin/gather_machineconfigdaemon_termination_logs &
+    queuedBackground $!
+  fi
+
+  # Gather vSphere resources. This is NOOP on non-vSphere platform.
+  if [[ ${OPTIONS[vsphere]} = true ]]; then
+    /usr/bin/gather_vsphere &
+    queuedBackground $!
+  fi
+
+  # Gather Performance profile information
+  if [[ ${OPTIONS[performance-profile]} = true ]]; then
+    /usr/bin/gather_ppc &
+    queuedBackground $!
+  fi
+
+  # Gather OSUS information
+  if [[ ${OPTIONS[osus]} = true ]]; then
+    /usr/bin/gather_osus &
+    queuedBackground $!
+  fi
+
+  # Gather ARO information
+  if [[ ${OPTIONS[aro]} = true ]]; then
+    /usr/bin/gather_aro
+  fi
+
+  if [[ ${OPTIONS[logs-crashloopbackoff]} = true ]]; then
     echoInfo "Started checking for CrashLoopBackOff pods"
     ensureAllPodsJSON
     INPUT="$(echo "${ALL_PODS_JSON}" | jq -r '.items[] | select(.status.containerStatuses[]?.state.waiting?.reason == "CrashLoopBackOff") | [.metadata.namespace, .metadata.name] | @tsv')"
-    if [ "${INPUT}" != "" ]; then
+    echoVerbose "Pods in CrashLoopBackOff: ${INPUT}"
+    if [[ "${INPUT}" != "" ]]; then
       while IFS=$'\t' read -r ns pod; do
         getPodLogs "${ns}" "${pod}"
       done <<< "${INPUT}" # Piping into the while loop would be a subshell and would lose any variable updates (e.g. BACKGROUND_PIDS)
@@ -196,21 +427,21 @@ defaultProcessing() {
     echoInfo "Finished checking for CrashLoopBackOff pods"
   fi
 
-  if [ ${OPTIONS[logs-etcd]} = true ]; then
+  if [[ ${OPTIONS[logs-etcd]} = true ]]; then
     echoInfo "Started checking for etcd pod logs"
     getPodLogsByAppName "etcd"
     getPodLogsByAppName "etcd-operator"
     echoInfo "Finished checking for etcd pod logs"
   fi
 
-  echoInfo "Checking if we should call any included scripts"
+  echoVerbose "Checking if we should call any included scripts"
 
   CURRENT_SCRIPT="$(basename "${0}")"
   for OTHER_SCRIPT in $(ls /usr/bin/gather*); do
     OTHER_SCRIPT_NAME="$(basename "${OTHER_SCRIPT}")"
-    if [ "${OTHER_SCRIPT_NAME}" != "${CURRENT_SCRIPT}" ]; then
+    if [[ "${OTHER_SCRIPT_NAME}" != "${CURRENT_SCRIPT}" ]]; then
       echoVerbose3 "Other script ${OTHER_SCRIPT_NAME} = ${OPTIONS[${OTHER_SCRIPT_NAME}]}"
-      if [ ${OPTIONS[${OTHER_SCRIPT_NAME}]} = true ]; then
+      if [[ ${OPTIONS[${OTHER_SCRIPT_NAME}]} = true ]]; then
         echoInfo "Executing script ${OTHER_SCRIPT_NAME}"
         #export BASE_COLLECTION_PATH="${MGOUTPUT}"
         /bin/bash "${OTHER_SCRIPT}"
@@ -218,4 +449,6 @@ defaultProcessing() {
       fi
     fi
   done
+
+  echoInfo "Finished defaultProcessing"
 }
--- collection-scripts/utilities/version.sh
+++ collection-scripts/utilities/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright IBM Corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-VERSION="0.1.20250723031"
+VERSION="0.1.20250729014"
